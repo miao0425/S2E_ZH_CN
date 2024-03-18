4763dddc2dc15874ada615cf26b115884a112203
miao0425 <162487258+miao0425@users.noreply.github.com>
2024-03-18 09:57:56 +0800

    v0.9.6

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,352 @@
+JEIEvents.hideItems(event => {
+	event.hide([
+		'exnihilosequentia:acacia_sieve',
+		'exnihilosequentia:spruce_sieve',
+		'exnihilosequentia:crimson_sieve',
+		'exnihilosequentia:jungle_sieve',
+		'exnihilosequentia:dark_oak_sieve',
+		'exnihilosequentia:birch_sieve',
+		'exnihilosequentia:warped_sieve',
+		'exnihilosequentia:mangrove_sieve',
+		'gag:time_sand_pouch',
+		'immersiveengineering:plate_copper',
+		'create:copper_sheet',
+		'mysticalagriculture:soulium_ingot',
+		'mysticalagriculture:soulium_gemstone',
+		'mysticalagriculture:soulium_seed_base',
+		'twilightforest:uncrafting_table',
+		'createaddition:gold_rod',
+		'createaddition:copper_rod',
+		'productivebees:expansion_box_bop_magic',
+		'extendedcrafting:handheld_table',
+		'cyclic:crafting_stick',
+		'cyclic:crafting_bag',
+		'exnihilosequentia:crimson_crucible',
+		'exnihilosequentia:warped_crucible',
+		'exmachinis:flux_compactor',
+		'constructionwand:stone_wand',
+		'ftbic:iron_furnace',
+		'cyclic:compressed_cobblestone',
+		'botania:apothecary_default',
+		'botania:apothecary_forest',
+		'botania:apothecary_plains',
+		'botania:apothecary_mountain',
+		'botania:apothecary_fungal',
+		'botania:apothecary_swamp',
+		'botania:apothecary_desert',
+		'botania:apothecary_taiga',
+		'botania:apothecary_mesa',
+		'botania:apothecary_mossy',
+		'botania:apothecary_deepslate',
+		'ae2:logic_processor',
+		'ae2:calculation_processor',
+		'ae2:engineering_processor',
+		'ae2:facade',
+		'farmersdelight:rich_soil',
+		'occultism:obsidian_dust',
+		'ftbic:obsidian_dust',
+		'ftbic:tin_dust',
+		'easy_villagers:iron_farm',
+		'easy_villagers:incubator',
+		'ae2:silicon',
+		'angelring:leadstone_angel_ring',
+		'angelring:hardened_angel_ring',
+		'angelring:reinforced_angel_ring',
+		'angelring:resonant_angel_ring',
+		'utilitix:experience_crystal',
+		'easy_villagers:iron_farm',
+		'easy_villagers:iron_farm',
+		'pamhc2foodcore:fruitsaladitem',
+		'thermal:ender_pearl_dust',
+		'mekanism:dust_quartz',
+		'ftbic:ender_dust',
+		'create:powdered_obsidian',
+		'ftbic:diamond_dust',
+		'infused_crystals:chargerblock',
+		'cyclic:crafter',
+		'eliquary:rending_gale',
+		'apotheosis:potion_charm',
+		'cyclic:chorus_flight',
+		'#pneumaticcraft:fluid_tanks',
+		'ftbic:gold_wire',
+		'ftbic:copper_wire',
+		'ftbic:aluminum_wire',
+		'kubejs:in_bloody_mechanism',
+		'kubejs:in_creative_blaze_cake',
+		'kubejs:in_starmetal_ingot',
+		'kubejs:in_enriched_iron',
+		'kubejs:in_rf_coil',
+		'cyclic:soundproofing_ghost',
+		'cyclic:obsidian_pressure_plate',
+		'quark:gold_bars',
+		'thermal:creosote_bucket',
+		'cyclic:packager',
+		'productivebees:configurable_honeycomb',
+		'ad_astra:compressor',
+		'compressium:soulsand_1',
+		'botanicalmachinery:mana_emerald_block',
+		'botanicalmachinery:mana_emerald',
+		'ftbic:aluminum_plate',
+		['solarflux:sp_6', 'solarflux:sp_7', 'solarflux:sp_8'],
+		[
+			'thermal:potion_cake',
+			'thermal:chocolate_cake',
+			'thermal:carrot_cake',
+			'thermal:spice_cake',
+		],
+		'productivebees:advanced_bop_magic_beehive',
+		[
+			'solarflux:sp_tf.fiery',
+			'solarflux:sp_tf.carminite',
+			'solarflux:photovoltaic_cell_4',
+			'solarflux:photovoltaic_cell_5',
+			'solarflux:photovoltaic_cell_6',
+		],
+		[
+			'moremekanismprocessing:crystal_ruby',
+			'moremekanismprocessing:shard_ruby',
+			'moremekanismprocessing:clump_ruby',
+			'moremekanismprocessing:dirty_dust_ruby',
+			'moremekanismprocessing:platinum_ingot',
+			'moremekanismprocessing:platinum_nugget',
+			'moremekanismprocessing:crystal_titanium',
+			'moremekanismprocessing:shard_titanium',
+			'moremekanismprocessing:clump_titanium',
+			'moremekanismprocessing:dust_titanium',
+			'moremekanismprocessing:titanium_ingot',
+			'moremekanismprocessing:titanium_nugget',
+			'moremekanismprocessing:crystal_bort',
+			'moremekanismprocessing:shard_bort',
+			'moremekanismprocessing:clump_bort',
+			'moremekanismprocessing:crystal_tungsten',
+			'moremekanismprocessing:shard_tungsten',
+			'moremekanismprocessing:clump_tungsten',
+			'moremekanismprocessing:dirty_dust_tungsten',
+			'moremekanismprocessing:tungsten_ingot',
+			'moremekanismprocessing:tungsten_nugget',
+			'moremekanismprocessing:dirty_dust_bort',
+			'moremekanismprocessing:dust_bort',
+			'moremekanismprocessing:gem_bort',
+			'moremekanismprocessing:crystal_sapphire',
+			'moremekanismprocessing:shard_sapphire',
+			'moremekanismprocessing:clump_sapphire',
+			'moremekanismprocessing:dirty_dust_sapphire',
+			'moremekanismprocessing:crystal_platinum',
+			'moremekanismprocessing:shard_platinum',
+			'moremekanismprocessing:clump_platinum',
+			'moremekanismprocessing:dust_platinum',
+			'moremekanismprocessing:dirty_dust_platinum',
+		],
+		[
+			'moremekanismprocessing:crystal_dilithium',
+			'moremekanismprocessing:shard_dilithium',
+			'moremekanismprocessing:clump_dilithium',
+			'moremekanismprocessing:dirty_dust_dilithium',
+			'moremekanismprocessing:dust_dilithium',
+			'moremekanismprocessing:gem_dilithium',
+			'moremekanismprocessing:crystal_peridot',
+			'moremekanismprocessing:shard_peridot',
+			'moremekanismprocessing:clump_peridot',
+			'moremekanismprocessing:dirty_dust_peridot',
+			'moremekanismprocessing:dust_peridot',
+			'moremekanismprocessing:gem_peridot',
+			'moremekanismprocessing:crystal_bismuth',
+			'moremekanismprocessing:shard_bismuth',
+			'moremekanismprocessing:clump_bismuth',
+			'moremekanismprocessing:dirty_dust_bismuth',
+			'moremekanismprocessing:dust_bismuth',
+			'moremekanismprocessing:bismuth_ingot',
+			'moremekanismprocessing:bismuth_nugget',
+		],
+		'thermal:coal_coke',
+		'thermal:coal_coke_block',
+		'mekanism:cardboard_box',
+		['ftbic:iridium_ore', 'ftbic:deepslate_iridium_ore'],
+		'ftbic:uranium_dust',
+		['ftbic:uranium_plate', 'ftbic:copper_plate'],
+		'sophisticatedbackpacks:copper_backpack',
+		,
+		,
+		,
+	])
+
+	event.hide(
+		/botanypotstiers:(?!elite_terracotta_botany_pot|elite_terracotta_hopper_botany_pot|ultra_terracotta_botany_pot|ultra_terracotta_hopper_botany_pot|creative_terracotta_botany_pot|creative_terracotta_hopper_botany_pot)/
+	)
+	event.hide(
+		/botanicalmachinery:(?!mechanical_apothecary|mechanical_runic_altar|mana_battery)/
+	)
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:wood",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:copper",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:bronze",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:silver",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:electrum",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:invar",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:steel",Throttle:1.0d}',
+	})
+	event.hide({
+		type: 'forge:nbt',
+		item: 'ironjetpacks:jetpack',
+		count: 1,
+		nbt: '{Id:"ironjetpacks:platinum",Throttle:1.0d}',
+	})
+})
+
+ItemEvents.tooltip(event => {
+	//Tooltips
+	event.add('exnihilosequentia:birch_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:acacia_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:dark_oak_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:jungle_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:crimson_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:spruce_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('exnihilosequentia:warped_sieve', [[Text.of('已禁用。改用橡木筛子').darkRed()]])
+	event.add('evilcraft:blood_infuser', [[Text.of('需要一些血来施展魔法').darkRed()]])
+	event.add('immersiveengineering:plate_aluminum', [
+		[Text.of('是铝(aluminum)，而不是(aluminium)').darkGray()],
+	])
+	event.add('kubejs:devilish_horn', [[Text.of('我全心全意地爱你').darkGray()]])
+	event.add('kubejs:devilish_horn', [[Text.of('无论发生什么').darkGray()]])
+	event.add('kubejs:reinforced_obsidian', [[Text.of('防凋零和龙').green()]])
+	event.add('pipez:basic_upgrade', [
+		[Text.of('§a物品:§f 8 Items/15t').gray()],
+		[Text.of('§b流体:§f 100 mB/t').gray()],
+		[Text.of('§5气体:§f 400 mB/t').gray()],
+		[Text.of('§6能量:§f 1024 FE/t').gray()],
+	])
+	event.add('pipez:improved_upgrade', [
+		[Text.of('§a物品:§f 16 Items/10t').gray()],
+		[Text.of('§b流体:§f 500 mB/t').gray()],
+		[Text.of('§5气体:§f 2000 mB/t').gray()],
+		[Text.of('§6能量:§f 8192 FE/t').gray()],
+	])
+	event.add('pipez:advanced_upgrade', [
+		[Text.of('§a物品:§f 32 Items/5t').gray()],
+		[Text.of('§b流体:§f 2000 mB/t').gray()],
+		[Text.of('§5气体:§f 8000 mB/t').gray()],
+		[Text.of('§6能量:§f 32,768 FE/t').gray()],
+	])
+	event.add('pipez:ultimate_upgrade', [
+		[Text.of('§a物品:§f 64 Items/t').gray()],
+		[Text.of('§b流体:§f 10000 mB/t').gray()],
+		[Text.of('§5气体:§f 40000 mB/t').gray()],
+		[Text.of('§6能量:§f 131,072 FE/t').gray()],
+	])
+	event.add('pipez:infinity_upgrade', [
+		[Text.of('§aItem, §bFluid, §5Gas, §6Energy:§f Bro... 阅读升级名称').gray()],
+	])
+	event.add('mob_grinding_utils:gm_chicken_feed_cursed', [
+		[Text.of('用邪恶点滴代替').green()],
+	])
+	event.add(
+		[
+			'ftbic:iridium_chunk',
+			'ftbic:iridium_ingot',
+			'kubejs:end_iridium',
+			'ftbic:iridium_ore',
+			'ftbic:deepslate_iridium_ore',
+		],
+		[[Text.of('在末地开采铱矿石可以找到第一颗铱').green()]]
+	)
+
+	//Removing Tooltips
+	event.addAdvanced('ars_nouveau:source_gem', (item, advanced, text) => {
+		text.remove(1)
+	})
+
+	event.addAdvanced('mob_grinding_utils:rotten_egg', (item, advanced, text) => {
+		text.remove(1), text.remove(2), text.remove(1), text.remove(2)
+	})
+
+	event.addAdvanced('mob_grinding_utils:dreadful_dirt', (item, advanced, text) => {
+		text.remove(1),
+			text.remove(2),
+			text.remove(3),
+			text.remove(1),
+			text.remove(2),
+			text.remove(3),
+			text.remove(1),
+			text.remove(2),
+			text.remove(3),
+			text.remove(1),
+			text.remove(2),
+			text.remove(3)
+	})
+
+	event.addAdvanced('mob_grinding_utils:gm_chicken_feed_cursed', (item, advanced, text) => {
+		text.remove(1), text.remove(2), text.remove(1), text.remove(2)
+	})
+})
+
+JEIEvents.information(event => {
+	event.addItem('deeperdarker:heart_of_the_deep', [
+		'如果不想花费大量魔力复制红心，先获得监守者刷怪蛋，然后将其放入刷怪笼中。这样就可以了',
+	])
+	event.addItem('kubejs:arcane_ore', ['MaxY: 96, MinY: -32'])
+	event.addItem('naturesaura:gold_leaf', [
+		'要获得它，闪耀纤维右键树叶。随着时间的推移，它将逐渐扩散。当树叶的颜色变成金黄色时，就可以收集，获得金叶',
+	])
+	event.addItem('thermal:blizz_powder', [
+		'暴雪粉现在也可以用流体灌装机制作。不要忽视这一点。你也可以击杀暴雪人掉落',
+	])
+	event.addItem('thermal:blitz_powder', [
+		'狂风粉现在也可以用流体灌装机制作。不要忽视这一点。你也可以击杀狂风人掉落',
+	])
+	event.addItem('thermal:basalz_powder', [
+		'岩石粉现在也可以用流体灌装机制作。不要忽视这一点。你也可以击杀岩石人掉落',
+	])
+	event.addItem('botania:ender_air_bottle', [
+		'你仍然可以末地中获得瓶装末地空气。添加冶金灌注机合成配方作为替代',
+	])
+	event.addItem('capsule:capsulemarker', ['禁用.'])
+	event.addItem('notenoughwands:acceleration_wand', ['禁用.'])
+	event.addItem('ftbic:antimatter', ['它在反物质构建器中生产'])
+	event.addItem('fluxnetworks:flux_dust', [
+		'在冶金灌注机中制作通量粉尘之前，您至少需要在基岩中制作一次',
+	])
+	event.addItem('immersiveengineering:storage_steel', [
+		'用热力系列的钢块代替。其功能相同',
+	])
+})
+
+JEIEvents.hideFluids(event => {
+	event.hide([Fluid.of('thermal:creosote'), Fluid.of('thermal:creosote_flowing')])
+})
+
+JEIEvents.hideFluids(event => {
+	event.hide(['thermal:creosote', 'thermal:creosote_flowing'])
+})
